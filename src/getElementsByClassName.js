8d47365dc1d236186e20d0de6cb6d59a618092f6
Manu <manu.mish@gmail.com>
2015-09-21 16:06:55 -0700

    Tried implementing JSOn stringify methods, with base cases still giving issues

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,16 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
+
+	//Return the body property - //document.body
+
+	//element.childNodes -- A NodeList object, representing a collection of nodes.
+
+	//element.classList -- Returns the number of classes in the list.
+	
+
+
+
+
   // your code here
 };
